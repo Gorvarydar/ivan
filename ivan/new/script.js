544cd08fe6292ae69d4bc018b653b6788578ba9d
Илья <bothisav@gmail.com>
2021-04-11 07:06:29 +0300

    lesson15-2

--- ivan/new/script.js
+++ ivan/new/script.js
@@ -7,8 +7,39 @@ const personalMovieDB = {
   privat: false
 
 };
+let i = 0;
+do{
+  const a = prompt("Один из последних просмотренных фильмов ?", ""),
+    b = prompt("На сколько оцените его ?", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+      i++
+    }
+          else {
+      console.log('error')
+      i--;
+    } 
+}
+while(i < 2);
+/* let i = 0;
+while(i < 2){
+  const a = prompt("Один из последних просмотренных фильмов ?", ""),
+    b = prompt("На сколько оцените его ?", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+      i++
+    }
+          else {
+      console.log('error')
+      i--;
+    }
+  
+  }
+ */
 
-for (i = 0; i < numberOfFilms; i++) {
+/* for (i = 0; i < 2; i++) {
   const a = prompt("Один из последних просмотренных фильмов ?", ""),
     b = prompt("На сколько оцените его ?", "");
   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
@@ -20,12 +51,12 @@ for (i = 0; i < numberOfFilms; i++) {
     i--;
   }
 
-}
+} */
 console.log(personalMovieDB);
 if(numberOfFilms < 10) {
   console.log('Просмотрено очень мало фильмов!');
 }
-else if(10 <= numberOfFilms <= 30) {
+else if(10 <= numberOfFilms && numberOfFilms <= 30) {
   console.log("Вы классический зритель!");
 }
 else if(numberOfFilms > 30)
