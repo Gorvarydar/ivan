469886c7a6a0dd6e4bb4b51c57331ce7f4523bed
Илья <bothisav@gmail.com>
2021-04-13 17:53:55 +0300

    lesson23

--- ivan/new/simple.js
+++ ivan/new/simple.js
@@ -4,4 +4,28 @@ let strObj = new String(str);
 // console.log(typeof(str));
 // console.log(typeof(strObj));
 
-console.dir([1, 2, 3]);
+// console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello:  function () {
+        console.log("Hello!")
+    }
+};
+
+const john = Object.create(soldier); //! создаём новый объект john , который буде прототипно наследоваться от soldier
+
+// const john = {
+//     health: 100
+// };
+
+// john.__proto__ = soldier; //* задание обекту свойств прототипа(Устаревшая форма записи!!!!!!!)
+// Object.setPrototypeOf(john, soldier);//! современная форма
+
+console.log(john);
+
+console.log(john.armor);
+
+john.sayHello();
+
