9467b08a4b084ce166554ec690e468d7abadef55
Илья <bothisav@gmail.com>
2021-04-14 22:44:04 +0300

    lessons 25-26

--- ivan/new/simple.js
+++ ivan/new/simple.js
@@ -29,3 +29,39 @@ console.log(john.armor);
 
 john.sayHello();
 
+//! ДИНАМИЧЕСКАЯ ТИПИЗАЦИЯ
+//? TO STRING
+
+console.log(typeof(String(null)));
+console.log(typeof(String(5)));//* превращение числа в строку при помощи String()
+console.log((5));
+
+console.log(typeof(5 + ''));//* превращение числа в строку при помощи конткатенации
+
+//? TO NUMBER
+
+console.log(typeof(Number('5'))); //* редкая команда транс-ть в число Number()
+
+console.log(typeof(+"6")); //* сщвременная форма унарный +
+
+console.log(typeof(parseInt("15px", 10))); //* parseInt(знач, система исчисления(тут десятичная))
+
+//? TO BOOLEAN
+
+// 0, null, NaN, undefined -//* это всё false, остальное будет true
+
+let varik = null;
+if(varik) {console.log(true)}; //*не сработает
+
+varik = 3;
+ 
+if(varik) {console.log(true)};//* ок
+
+
+console.log(typeof(Boolean(5))); //* Boolean() устар
+
+console.log(typeof(!! 5));
+
+
+
+
