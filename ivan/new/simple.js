e81da2262df1b37477f11c6407ba8d8b9f8f196a
Илья <bothisav@gmail.com>
2021-04-22 08:53:23 +0300

    lesson 37

--- ivan/new/simple.js
+++ ivan/new/simple.js
@@ -1,8 +1,23 @@
-"use string"
+const btns = document.querySelectorAll("button");
 
-const p = document.querySelectorAll('p');
-console.log(p);
+console.log(btns[0].classList.length); //*обращение к конкретному элементу псевдомассива btns[0]
+                                    //*classList.length колличество его свойств
 
-const script = document.createElemen('script');
-script.src = "new/test.js";
-document.body.append(script);
+console.log(btns[0].classList.item(0)) //* обращение к классу под индек 0 выведется blue если 1 some
+                    /* <button class="blue some"></button> */
+ console.log(btns[1].classList.add('red')); //*add() метод добавления классов можно через , добавить несколько
+ console.log(btns[0].classList.remove('blue')); //*remove() удаляет классы
+// console.log(btns[0].classList.toggle('blue')); ///*toggle() добавляет класс если его нет,но если такjй класс есть не добавляет
+
+if(btns[1].classList.contains('red')) {  //* метод contains() проверяет есть ли такой класс
+    console.log('red');
+};
+// btns[0].addEventListener('click',() => {
+//     if(!btns[1].classList.contains('red')) {     //*разное исполнение одной и той же задачи
+//      btns[1].classList.add('red')}
+//      else{btns[1].classList.remove('red')};  
+// });
+
+btns[0].addEventListener('click', () => {      //* разное исполнение одной и той же задачи
+    btns[1].classList.toggle('red');
+});
