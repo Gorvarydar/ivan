135be196908ac1d6bced472568e00f0388058d7b
Илья <bothisav@gmail.com>
2021-04-30 08:28:07 +0300

    042

--- ivan/new/simple.js
+++ ivan/new/simple.js
@@ -1,2 +1,35 @@
-const da = new Date('2008-04-20');
-console.log(Date.parse(da));
+ 'use strict'
+  
+ const box = document.querySelector('.box');
+ const btn = document.querySelector('button');
+//  const widht = box.clientWidth;
+//  const height = box.clientHeight;
+
+// const widht = box.offsetWidth;
+// const height = box.offsetHeight;
+
+const widht = box.scrollWidth;
+const height = box.scrollHeight;
+
+ console.log(widht, height);
+
+ btn.addEventListener('click', () => {
+    //  box.style.height = box.scrollHeight  + 'px';   //*(инлайн стили) при обращении к стилям мы получаем число ,чтобы изменения корректно изменились
+   console.log(box.scrollTop);                                         //*   нужно добавить формат измерения в данном случае 'px'
+});                     
+
+console.log(box.getBoundingClientRect()); //* получает все координаты ,который есть у элемента
+console.log(box.getBoundingClientRect().top) //* имеет разные значения их можно получать отдельно(прим. top)
+//* computed-стили (в конкретном выражении , которые применились.Изменить нельзя ,тоько прочитать)
+
+const style = window.getComputedStyle(box);//* получение объекта со всеми его свойствами
+console.log(style.display); //*получение конкретного свойства
+//? работать с псевдоэлементами в JS  не т возможности , но возможно получить свойства псевдоэлементов
+//? инлайн стили приоритетнее чем computed  стили
+
+document.documentElement.scrollTop = 0; //* обращение ко всей странице как одному объекту
+window.scrollBy(0, 400); //* проскролить относительно  положения пользователя
+window.scrollTo(0, 400); //* проскролить относительно всей страницы
+
+
+
