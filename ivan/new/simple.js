fe8426e0c6dc4e178ff984dd8d4cc0833db618ff
Илья <bothisav@gmail.com>
2021-05-10 12:26:35 +0300

    some

--- ivan/new/simple.js
+++ ivan/new/simple.js
@@ -1,36 +1,7 @@
-'use strict'
-
-const usd = document.querySelector('.usd'),
-      rub = document.querySelector('.rub');
-
-      usd.addEventListener('input', () => {
-          const request = new XMLHttpRequest();
-
-        //   request.open(method, url, async, login, password) //*  open() - метод собирает настройки которые помогут сделать зпрос
-        //                                    //* 1 аргумент -метод для запроса,
-        //                                    //*2-й адрес по которому будет сделан запрос
-        //                                    //*3-й вид запроса async (значит асинхронность , в ajax gпо умолчанию true )
-        //                                    //*4-й логин (необязательные)
-        //                                    //*5-й пароль (необязательные)
-            //? самый распространённе методы GET(получить с сервера) и POST(отправить на сервер)
-            request.open('GET','new/current.json');
-            request.setRequestHeader('Content-type','application/json; charset=utf-8'); //* http заголовки уточняем какой именно будет запрос
-            request.send()
-            request.addEventListener('readystatechange', () => {
-                if (request.readyState === 4 && request.status === 200) {
-                    console.log(request.response); 
-                }
-            });
-
-            //? свойства запроса
-            // status//* состояние объекта КОД(каждый код что-то значит например 404 - клиентская ошибка)
-            // statusText //* текстовое описание ответа от сервера
-            // response //* ответ от сервера(то что мы используем на клиетне)
-            // readystate //* текущее состояние запроса( от 0 до 4)
-
-            //*события запроса
-        //    loadstart
-        //    progress
-        //    abort
-        //    loadend
-      })
+// const text = prompt('как ваше имя?');
+// alert("Nice to meet you " + text);
+const arr = ['hi', 'dear', 'world'];
+console.log(arr[0] + arr[1] + arr[2]);
+console.log(arr.join('-'));
+console.log(arr.slice(2));
+console.log(`${arr[0]}, ${arr[1]}, ${arr[2]}`);
